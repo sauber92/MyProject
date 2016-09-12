0659206bcfdf1988c7aeaf4af131ff1c9a95e42c
정준영 <jjy920517@gmail.com>
2016-09-13 02:28:17 +0900

    Node.js 웹앱 만들기 - 본문 읽기

--- Node_js/app_file.js
+++ Node_js/app_file.js
@@ -53,3 +53,20 @@ app.post('/topic', function(req, res) {
     res.send('Sucess!');
   });
 });
+app.get('/topic/:id', function(req, res) {
+  var id = req.params.id;
+
+  fs.readdir('data', function(err, files) {
+    if(err) {
+      console.log(err);
+      res.status(500).send('Internal Server Error');
+    }
+    fs.readFile('data/'+id, 'utf8', function(err, data) {
+      if(err) {
+        console.log(err);
+        res.status(500).send('Internal Server Error');
+      }
+      res.render('view', {topics:files, title:id, description:data});
+    });
+  });
+});
